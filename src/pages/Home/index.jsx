52a3dac8e1a38e3844c60459d7f61e369e65ae02
francogaray <francogaray.dev@gmail.com>
2023-06-06 01:19:14 -0300

    fectch y render cards from fake Api

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
 import Card from "../../components/Card";
 
 const Home = () => {
-    return <div className="bg-green-400">
-        HOME
-        <Card/>
+
+    const [item, setItem] = useState ([])
+
+    useEffect( () => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+        .then(res => res.json() )
+        .then(data => setItem(data))
+    },[] )
+
+    return <div className="grid gap-4 grid-cols-4 w-full max-w-screen-xl bg-green-400">
+        {item?.map( (item) => (
+            <Card data={item} key={item.id} />
+        ))
+
+        }
     </div>;
 };
 
